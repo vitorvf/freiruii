f5f512cdd7f905e0186503801e1c7ed43a458304
Vitor Ferreira <vitorferreiravf2@gmail.com>
2022-02-08 20:51:46 -0300

    Novas telas editar categoria/ post

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var tooltipTriggerList = [].slice.call(
+  document.querySelectorAll('[data-toggle="tooltip"]'),
+);
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl);
+});
